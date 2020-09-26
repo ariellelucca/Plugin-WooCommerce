a921896a3b344ba45a67044d6aec49302170eaed
Arielle Lucca <a.lucca@hotmail.com.br>
2020-09-26 01:18:47 -0300

    Pendente apenas alterar o texto do e-mail

---
+++ wp-content/plugins/new_status/includes/custom-mail.php
@@ -0,0 +1,149 @@
+<?php
+
+// Se for acessado diretamente, não executa
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+// Classe que extende WC_Email e dá acesso aos métodos da classe de email do WooCommerce
+class WC_Expedited_Order_Email extends WC_Email {
+    // Construtor
+    public function __construct() {
+
+        // Define o ID da configuração
+        $this->id = 'wc_expedited_order';
+
+        // Define o título da configuração
+        $this->title = 'Expedited Order';
+
+        // Define a descrição da configuração
+        $this->description = 'Expedited Order Notification';
+
+        // Define o cabeçalho e a mensagem
+        $this->heading = 'Expedited Order';
+        $this->subject = 'Expedited Order';
+
+        // Define os layouts de envio de email
+        $this->template_html  = 'emails/admin-new-order.php';
+        $this->template_plain = 'emails/plain/admin-new-order.php';
+
+        // Define o email do cliente como verdadeiro
+        $this->customer_email = true;
+
+        // Dispara o email quando o status for 'enviado'. 
+        add_action( 'woocommerce_order_status_pending_to_sent', array( $this, 'trigger' ), 10, 2 );;
+        add_action( 'woocommerce_order_status_processing_to_sent', array( $this, 'trigger' ), 10, 2 );
+        add_action( 'woocommerce_order_status_failed_to_sent', array( $this, 'trigger' ), 10, 2 );
+        add_action( 'woocommerce_order_status_on-hold_to_sent', array( $this, 'trigger' ), 10, 2 );
+        add_action( 'woocommerce_order_status_cancelled_to_sent', array( $this, 'trigger' ), 10, 2 );
+        add_action( 'woocommerce_order_status_completed_to_sent', array( $this, 'trigger' ), 10, 2 );
+
+        // Chama o construtor
+        parent::__construct();
+
+        // Pega as informações do destinatário
+        $this->recipient = $this->get_option( 'recipient' );
+
+        // Se não há destinatário configurado, envia para o próprio admin
+        if ( ! $this->recipient )
+            $this->recipient = get_option( 'admin_email' );
+      
+    }
+
+
+    public function get_default_subject() {
+        return __( '[{site_title}]: Expedited order #{order_number}', 'woocommerce' );
+    }
+
+    public function get_default_heading() {
+        return __( 'Expedited order: #{order_number}', 'woocommerce' );
+    }
+
+    public function get_default_additional_content() {
+        return __( 'Your order has been expedited.', 'woocommerce' );
+    }
+
+    public function trigger( $order_id, $order = false ) {
+        $this->setup_locale();
+
+        if ( $order_id && ! is_a( $order, 'WC_Order' ) ) {
+            $order = wc_get_order( $order_id );
+        }
+
+        if ( is_a( $order, 'WC_Order' ) ) {
+            $this->object                         = $order;
+            $this->placeholders['{order_date}']   = wc_format_datetime( $this->object->get_date_created() );
+            $this->placeholders['{order_number}'] = $this->object->get_order_number();
+        }
+
+        if ( $this->is_enabled() && $this->get_recipient() ) {
+            $this->send( $this->get_recipient(), $this->get_subject(), $this->get_content(), $this->get_headers(), $this->get_attachments() );
+        }
+
+        $this->restore_locale();
+    }
+    
+    // Função que seleciona o template/conteúdo do email em html
+    public function get_content_html() {
+        ob_start();
+        woocommerce_get_template( $this->template_html, array(
+            'order'         => $this->object,
+            'email_heading' => $this->get_heading()
+        ) );
+        return ob_get_clean();
+    }
+
+    // Função que seleciona o template/conteúdo do email em texto plano
+    public function get_content_plain() {
+        ob_start();
+        woocommerce_get_template( $this->template_plain, array(
+            'order'         => $this->object,
+            'email_heading' => $this->get_heading()
+        ) );
+        return ob_get_clean();
+    }
+
+    // Função que inicializa os campos do formulário
+    public function init_form_fields() {
+
+        $this->form_fields = array(
+            'enabled'    => array(
+                'title'   => 'Enable/Disable',
+                'type'    => 'checkbox',
+                'label'   => 'Enable this email notification',
+                'default' => 'yes'
+            ),
+            'recipient'  => array(
+                'title'       => 'Recipient(s)',
+                'type'        => 'text',
+                'description' => sprintf( 'Enter recipients (comma separated) for this email. Defaults to <code>%s</code>.', esc_attr( get_option( 'admin_email' ) ) ),
+                'placeholder' => '',
+                'default'     => ''
+            ),
+            'subject'    => array(
+                'title'       => 'Subject',
+                'type'        => 'text',
+                'description' => sprintf( 'This controls the email subject line. Leave blank to use the default subject: <code>%s</code>.', $this->subject ),
+                'placeholder' => '',
+                'default'     => ''
+            ),
+            'heading'    => array(
+                'title'       => 'Email Heading',
+                'type'        => 'text',
+                'description' => sprintf( __( 'This controls the main heading contained within the email notification. Leave blank to use the default heading: <code>%s</code>.' ), $this->heading ),
+                'placeholder' => '',
+                'default'     => ''
+            ),
+            'email_type' => array(
+                'title'       => 'Email type',
+                'type'        => 'select',
+                'description' => 'Choose which format of email to send.',
+                'default'     => 'html',
+                'class'       => 'email_type',
+                'options'     => array(
+                    'plain'     => 'Plain text',
+                    'html'      => 'HTML', 'woocommerce',
+                    'multipart' => 'Multipart', 'woocommerce',
+                )
+            )
+        );
+    }
+}
--- wp-content/plugins/new_status/main.php
+++ wp-content/plugins/new_status/main.php
@@ -7,9 +7,24 @@
  * Author URI: https://github.com/ariellelucca
  */
 
+// Função de internacionalização
+function new_status_init(){
+    $plugin_dir = basename(dirname(__FILE__)).'/languages';
+    load_plugin_textdomain('new_status', false, $plugin_dir);
+}
+add_action('plugins_loaded', 'new_status_init');
+
+// Função que adiciona o css do plugin
+function new_status_stylesheet() {
+    $plugin_dir = plugin_dir_url( __FILE__ );
+
+    wp_enqueue_style( 'new_status_style',  $plugin_dir . "/css/style.css");
+}
+add_action( 'wp_enqueue_scripts', 'new_status_stylesheet' );
+
 // Função para criar o status 'Enviado'
-function register_status_enviado(){
-    register_post_status('wc-status-enviado', array(
+function register_status(){
+    register_post_status('wc-sent', array(
         'label'                     => 'Enviado',
         'public'                    => true,
         'exclude_from_search'       => false,
@@ -19,13 +34,11 @@ function register_status_enviado(){
     ) );
 }
 
-// Inicia a função register_status_enviado()
-add_action('init', 'register_status_enviado');
-
-
+// Inicia a função register_status()
+add_action('init', 'register_status');
 
 // Função para criar o status personalizado
-function add_status_enviado($array_status) {
+function add_sent_status($array_status) {
  
     // Array auxiliar para a chave do status
     $array_status_aux = array();
@@ -34,12 +47,92 @@ function add_status_enviado($array_status) {
     foreach ($array_status as $key => $status) {
         $array_status_aux[$key] = $status;
         if ('wc-processing' === $key) {
-            $array_status_aux['wc-status-enviado'] = 'Enviado';
+            $array_status_aux['wc-sent'] = 'Sent';
         }
     }
-
-    
+   
     return $array_status_aux;
 }
-add_filter('wc_order_statuses', 'add_status_enviado');
+add_filter('wc_order_statuses', 'add_sent_status');
 
+// Função que mostra o status no front end
+function show_status($order_id){
+    echo '<h2>'. esc_html__( 'Order Status', 'new_status' ). '</h2>';
+    echo '<div class="center">';
+    echo '<div class="order-status">';
+    echo '<div class="order-status-timeline">';
+    echo '<div class="order-status-timeline-completion"></div>';
+    echo '</div>';
+    $order_id = $order_id['ordem'];
+    $my_order = wc_get_order( $order_id );
+    
+    // Array com os status
+    $statuses = array(
+        'pending',
+        'processing',
+        'on-hold',
+        'sent',
+        'completed'
+    );
+
+    $done = true;
+
+    // Percorre o array de status até encontrar o atual
+    $percent = 0;
+    foreach ( $statuses as $status ) {
+        if ( $done ) {
+            $left = $percent;
+            $aux = '';
+            $status_name = wc_get_order_status_name( $status );
+            echo '<div class="image-order-status image-order-status-new img-circle" style="left: '.$left.'%">';
+            echo '<div class="icon"><i class="fas fa-check"></i></div>';
+            echo '<span class="status">'.$status_name.'</span>';
+            echo '</div>';
+        }
+    
+        if ( $status === $my_order->get_status() ) {
+            $done = false;            
+        }
+
+        if ($done === false){
+            if ($status !== $my_order->get_status()){
+                $left = $percent;
+                $aux = '';
+                $status_name = wc_get_order_status_name( $status );
+                echo '<div class="image-order-nokstatus image-order-status-new img-circle" style="left: '.$left.'%">';
+                echo '<div class="icon"></div>';
+                echo '<span class="status">'.$status_name.'</span>';
+                echo '</div>';
+            }
+        }
+        // adiciona a distancia entre cada div de status
+        $percent += 23;
+    }
+    echo '</div>';
+    echo '</div>';
+}
+add_shortcode( 'shortcode_status', 'show_status' );
+
+// Função de emails para novo status
+function add_custom_mail_woocommerce( $email_classes ) {
+    // Inclui a classe
+    require( 'includes/custom-mail.php' );
+
+    // Adiciona o email para as classes de email do WooCommerce
+    $email_classes['WC_Expedited_Order_Email'] = new WC_Expedited_Order_Email();
+
+    return $email_classes;
+
+}
+add_filter( 'woocommerce_email_classes', 'add_custom_mail_woocommerce' );
+
+
+function email_subject_sent_order( $formated_subject, $order ){
+    // Executa se a ordem tiver status 'enviada'
+    if( $order->has_status('sent') ) 
+        return  $formated_subject; 
+
+    return sprintf( esc_html__( 'Your order had been expedited #%s', 'textdomain'), $order->get_id() );
+    
+}
+add_filter( 'woocommerce_email_subject_sent_order', 'email_subject_sent_order', 10, 2 );
